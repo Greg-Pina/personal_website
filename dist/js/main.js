2ab3dafaae0459d1f2599dd8c8c2f412a4ba9f0b
Greg Pina <pinagm2825@gmail.com>
2019-02-10 21:21:01 -0500

    Fixing color saturation

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,7 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const mainPage = document.querySelectorAll('.main');
 
 // Set Initial State Of Menu
 let showMenu = false;
